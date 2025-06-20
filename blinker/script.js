9f5c1ebc3c85eb3a7b7d0de84a2c6c89f8c20776
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-06-20 21:52:39 +0000

    Jun 20, 2025, 5:52 PM

--- blinker/script.js
+++ blinker/script.js
@@ -39,6 +39,8 @@ document.addEventListener('DOMContentLoaded', () => {
     function startCountdown(plot, index) {
         const overlay = document.getElementById('countdown-overlay');
         const text = document.getElementById('countdown-text');
+        overlay.style.backgroundColor = 'black';
+        overlay.style.opacity = '0.5';
         let values = ['Ready', 'Set', 'Go'];
         let i = 0;
 
