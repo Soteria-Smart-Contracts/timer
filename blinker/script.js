0f2abae3ec2e8f0c4ffd3f6aa21d089e6f06468e
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-06-20 19:34:55 +0000

    Jun 20, 2025, 3:34 PM

--- blinker/script.js
+++ blinker/script.js
@@ -125,7 +125,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }, 200);
 
-        overlay.style.backgroundColor - 'black';
+        overlay.style.backgroundColor = 'black';
         text.style.color = 'white';
 
     }
