762214891e70a3cfeeed2ec9eee545cef0b2c960
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-06-20 18:23:30 +0000

    Jun 20, 2025, 2:23 PM

--- blinker/script.js
+++ blinker/script.js
@@ -87,6 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
             plot.classList.remove('active');
             plot.removeChild(timerElement);
+            isBlinking = false; // Allow another blink
           }, 120000); // 2 min
         }
       }, 100);
