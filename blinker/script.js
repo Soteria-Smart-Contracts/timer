ec784c11fccd5c0e422802965a3aec9cb25c1e3c
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-06-21 00:25:09 +0000

    Jun 20, 2025, 8:25 PM

--- blinker/script.js
+++ blinker/script.js
@@ -1,5 +1,5 @@
 const plots = document.querySelectorAll('.plot');
-let treeStates = [];
+let treeStates = [false, false, false, false, false, ];
 let totalBlinkersToday = 0;
 let highScore = 0;
 let isBlinking = false;
