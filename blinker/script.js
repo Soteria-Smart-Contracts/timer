01cbdf845cca63d7bd3107cb3e764c182a91baca
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2025-06-28 01:42:52 +0000

    Jun 27, 2025, 9:42 PM

--- blinker/script.js
+++ blinker/script.js
@@ -167,7 +167,6 @@ function displayGnome() {
     gnomeContainer.style.marginTop = '10px';
 
     gnomeContainer.appendChild(img);
-    gnomeContainer.appendChild('<br>');
     gnomeContainer.appendChild(document.createTextNode(message));
 
     text.innerHTML = ''; // Clear existing text content
